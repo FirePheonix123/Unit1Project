7ed4c506173c5d52a788241eb8f81c0e3db5a936
Yuhao <Yguan1996@bths.edu>
2023-09-21 10:20:01 -0400

    intial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+
+        System.out.println("Welcome to the  tip Calculator!");
+        System.out.println("How many people are in your group: ");
+
+        System.out.println("What's the tip percentage? (0-100): ");
+
+        System.out.println("Enter a cost in dollars and cents, e.g. 12.50 (-1 to end): ");
+        System.out.println("Enter the item: ");
+
+        System.out.println("-------------------------------");
+        System.out.println("Total bill before tip: ");
+        System.out.println("Total percentage: ");
+        System.out.println("Total tip: ");
+        System.out.println("Total bill with tip: ");
+        System.out.println("Per person cost beofre tip: ");
+        System.out.println("Tip per person: ");
+        System.out.println("Total cost per person: ");
+        System.out.println("-------------------------------");
+        System.out.println("Items ordered:");
+
+
+        }
+    }
+}
